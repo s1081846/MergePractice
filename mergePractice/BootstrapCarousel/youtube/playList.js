1929cc19207374a2da47f96ce86b7d51439cb9b3
Kenny Su <s1081846@mail.yzu.edu.tw>
2021-06-26 12:14:51 +0800

    goddd

--- mergePractice/BootstrapCarousel/youtube/playList.js
+++ mergePractice/BootstrapCarousel/youtube/playList.js
@@ -1,12 +1,12 @@
 //YouTube Video ID
 let playList = [
     "_zEebCcYPGs",
-    "HXf4Ltggjfw",
+    "fb-a1Sp6TO0",
     "c6gi6IP6ay4"
     ];
     //播放起訖秒數
     let playTime = [
     [117,147],
-    [144,150],
+    [69,91],
     [146,152]
     ];
